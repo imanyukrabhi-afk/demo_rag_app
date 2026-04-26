a30b8e3e2f2299b5c297d93688eeeed95304f9f1
imanyukrabhi-afk <imanyu.krabhi@gmail.com>
2026-04-27 03:45:28 +0530

    Add main application class for Rag Document Q&A

---
+++ src/main/java/com/rag/RagDocQaApplication.java
@@ -0,0 +1,15 @@
+package com.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for RAG Document Q&A Application
+ */
+@SpringBootApplication
+public class RagDocQaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagDocQaApplication.class, args);
+    }
+}
